2a6adfb504d4127bb97d5d2a05d971067d0d77ca
thocodeanhquan <thocodeanhquan@gmail.com>
2023-07-30 03:43:56 +0700

    Hoàn thiện Service tải nhạc online :<

--- app/src/main/java/com/example/myapplication/adapter/SongListAdapter.java
+++ app/src/main/java/com/example/myapplication/adapter/SongListAdapter.java
@@ -1,13 +1,20 @@
 package com.example.myapplication.adapter;
 
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.Environment;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.myapplication.R;
 import com.example.myapplication.activity.PlayerActivity;
@@ -22,6 +29,9 @@ public class SongListAdapter extends BaseAdapter implements AdapterView.OnItemCl
 
     ArrayList<Song> listSong;
     Context context;
+    DownloadManager downloadManager;
+    long enqueu;
+
 
     public SongListAdapter(Context context) {
         listSong = new ArrayList<>();
@@ -50,6 +60,36 @@ public class SongListAdapter extends BaseAdapter implements AdapterView.OnItemCl
         TextView songName = view.findViewById(R.id.song_name);
         TextView songSinger = view.findViewById(R.id.song_singer);
         ImageView songImage = view.findViewById(R.id.song_image);
+        Button btn_download = view.findViewById(R.id.btn_download);
+
+        BroadcastReceiver receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if(DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)){
+                    Toast.makeText(context, "Đã tải nhạc xong thưa ngài!", Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+
+        context.registerReceiver(receiver, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+        btn_download.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(listSong.get(i).getSongUrl()));
+
+
+                request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE | DownloadManager.Request.NETWORK_WIFI)
+                        .setTitle(listSong.get(i).getSongName() + ".mp3")
+                        .setDescription(listSong.get(i).getSongName()+"-"+listSong.get(i).getSongSinger())
+                        .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, listSong.get(i).getSongName() + ".mp3");
+                enqueu = downloadManager.enqueue(request);
+                Intent intent = new Intent();
+                intent.setAction(DownloadManager.ACTION_VIEW_DOWNLOADS);
+                context.startActivity(intent);
+            }
+        });
 
         songName.setText(listSong.get(i).getSongName());
         songSinger.setText(listSong.get(i).getSongSinger());
